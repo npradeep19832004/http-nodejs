352fdce913e5748cb5b6adff81d4d62dfbf0a6e7
npradeep19832004 <79438428+npradeep19832004@users.noreply.github.com>
2022-12-13 22:55:44 +0530

    html update

--- server.mjs
+++ server.mjs
@@ -4,3 +4,23 @@ createServer((req, res) => {
   res.write('Hello Dear World!');
   res.end();
 }).listen(process.env.PORT);
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id="demo">Click the button to change the layout of this paragraph</p>
+
+<button onclick="myFunction()">Click Me!</button>
+
+<script>
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
+</script>
+
+</body>
+</html>
+
